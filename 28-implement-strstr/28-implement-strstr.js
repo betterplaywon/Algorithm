3448fa1da633bc1dc8e27fa75fe96046028220d8
강지원 <78709765+betterplaywon@users.noreply.github.com>
2022-07-05 22:16:07 +0900

    Time: 75 ms (71.21%), Space: 41.8 MB (82.36%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+var strStr = function(haystack, needle) {
+    if(needle === 0) return 0;
+    
+    for(let i = 0; i < haystack.length; i++) {
+        if(haystack[i] === needle[0]) {
+           const cutHaystack = haystack.slice(i, i+needle.length);
+            if(cutHaystack === needle) {
+                return i;
+            }
+         }
+    }
+  return -1;
+};
