445b4d2b38af0ee4aa1903665fd80858ffcfcfe3
강지원 <78709765+betterplaywon@users.noreply.github.com>
2022-07-21 23:04:20 +0900

    Time: 75 ms (84.65%), Space: 42 MB (96.70%) - LeetHub

---
+++ 67-add-binary/67-add-binary.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+  // 첫 시도 : const result = BigInt(a).toString(2) + BigInt(b).toString(2);
+// stackoverflow 질문 중 BigInt에 0b를 추가해줌으로서 원하는 결과값을 얻을 수 있었다는 내용 참조
+
+var addBinary = function(a, b) {
+     const aBigInt = `0b${a}`
+  const bBigInt = `0b${b}`
+  const sum = BigInt(aBigInt) + BigInt(bBigInt)
+  return sum.toString(2)
+};
