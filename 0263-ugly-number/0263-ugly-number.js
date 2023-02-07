e41ddd9c07f1f21cbfdb3395165101a6ffb1cb6d
강지원 <78709765+betterplaywon@users.noreply.github.com>
2023-02-07 16:54:48 +0900

    Time: 83 ms (41.60%), Space: 42.9 MB (49.61%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -4,10 +4,7 @@
  */
 var isUgly = function(n) {
     if(!n) return false;
-    // if(n === 1) return true;
-    
-    // const primeFactors = [2,3,5];
-    // for(let i=0; i<primeFactors.length; i++) {} 
+   
     while (n % 2 === 0) n /= 2;
     while (n % 3 === 0) n /= 3;
     while (n % 5 === 0) n /= 5;
