3996577eaa23c9fa5bd9e650fb734804f9ee04dc
강지원 <78709765+betterplaywon@users.noreply.github.com>
2023-02-23 11:48:35 +0900

    Time: 228 ms (42.13%), Space: 51.7 MB (6.74%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n === 0) return false;
+    
+    if(n === 1 || n/3 === 1) return true;
+    else if(n%3 === 0) return isPowerOfThree(n/3);
+    else return false;
+ 
+};
