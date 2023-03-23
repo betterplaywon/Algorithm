64f76b153f64ae97ff899826e0e1db49f16fb675
강지원 <78709765+betterplaywon@users.noreply.github.com>
2023-03-23 15:24:46 +0900

    Time: 57 ms (97.21%), Space: 44.8 MB (32.50%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    
+    if(nums[0] === 1 && nums.length === 1) return 1;
+    
+    let newNum = 0;
+    let maxNum = 0;
+    
+    // 현재 idx가 1이고 다음 idx가 1이면 newNum을 1씩 올리고, 그게 아니라면 newNum을 초기화시킨다.
+    
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i]===1) {
+         newNum+=1;
+            
+            if(maxNum < newNum){
+              maxNum = newNum;
+            }
+            
+        } else {
+         newNum=0;
+        }
+    }
+    
+   return Math.max(newNum, maxNum);
+};
+
+
