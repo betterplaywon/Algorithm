617faa5c536e159a60234da178eb42bd78a3a414
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-01-08 06:38:52 +0900

    Time: 238 ms (5.14%), Space: 52.9 MB (5.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,40 +2,32 @@
  * @param {string} s
  * @return {number}
  */
-
-// 숫자가 크거나 같을 땐 누적합산
-// 만약 다음 수가 없다면 현재의 숫자만
-
-
- const romanObj = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
+var romanToInt = function(s) {
+    
+    const romanMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
 
-const romanToInt = function(s) {
-
-let convertRomanInteger = 0;
-
-   if(!s) {
-       return null;
-   }
-
-for(let i = 0; i < s.length; i++) {
-   const num = romanObj[s[i]];
-   const nextNum = romanObj[s[i+1]];
-
-    if(num >= nextNum) {
-        convertRomanInteger += num;
-    } else {
-        if(!nextNum) return convertRomanInteger + num;
-            // num < nextNum
-            convertRomanInteger -= num;
-            }
+    let sum=0;
+    for(let i=0; i<s.length; i++) {
+        let now = romanMap[s[i]];
+        let next = romanMap[s[i+1]];
+      
+          console.log('바깥 i: ',i);
+        
+        if(now<next) {
+            sum+=next-now;
+            i++;
+            console.log('for문 내부 i: ',i);
+        } else {
+            sum+=now;
         }
-    return convertRomanInteger;
     }
+    return sum;
+};
