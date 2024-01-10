1ab2b84a3b44357f7268bd29e64c0133f179c08f
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-01-10 23:29:10 +0900

    Time: 53 ms (70.58%), Space: 42.8 MB (29.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,24 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    // str 요소를 순차적으로 접근한다
-    // 순차적으로 접근한 요소의 index를 뽑는다
-    // 현재 요소와 다음 요소의 index를 비교한다
-    // 중복되는 요소의 [i]와 [j]를 리턴하고, 중복되지 않는다면 "" 를 리턴한다.
+    if(strs.length === 0) return '';
+    let result = '';
+    const firstWord = strs[0];
     
-    let result = "";
-    const firstStrs = strs[0];
-    
-    for(let i=0; i<firstStrs.length; i++) {
-        const selectStrs = firstStrs[i];
-        for(let j=1; j<strs.length; j++) {
-            // 처음부터 중복되어 바로 다음으로 넘어가는게 반복되어 output에 f만 출력.
-           // if(strs[j][i] === selectStrs) return result += selectStrs;
-             if(strs[j][i] !== selectStrs) return result;
-        }
-        result += selectStrs;
+    for(let i=0; i<firstWord.length; i++){
+        const element = firstWord[i];
+         for(let j=1; j<strs.length; j++) {
+           if(strs[j][i] !== element) return result;
+            }
+        result += element;
     }
     return result;
 };
-
-strs = ["flower","flow","flight"]
