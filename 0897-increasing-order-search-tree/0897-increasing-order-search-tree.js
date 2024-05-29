eb9f87338419bda228048306c6b84e72cb25afb4
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-05-29 20:54:15 +0900

    Time: 75 ms (7.47%), Space: 52.4 MB (5.69%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+   
+    let newTree= new TreeNode();
+    let node = newTree;
+    
+    const traverse = (root) => {
+        if(!root) return;
+        traverse(root.left);
+        node.right = root;
+        node = node.right;
+        console.log(node.right);
+        node.left = null;
+        traverse(root.right);
+    }
+    traverse(root);
+    return newTree.right;
+};
