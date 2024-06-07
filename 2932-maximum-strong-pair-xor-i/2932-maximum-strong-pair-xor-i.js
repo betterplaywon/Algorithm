ed65ba79c433da4fdad2bbe8985e68b6a74e50f8
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-06-07 14:35:03 +0900

    Time: 71 ms (40.61%), Space: 50.5 MB (90.30%) - LeetHub

---
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumStrongPairXor = function(nums) {
+    let result = 0;
+    
+    for(let i=0; i<nums.length; i++) {
+        for(let j=0; j<nums.length; j++) {
+            if(Math.abs(nums[i]-nums[j]) <= Math.min(nums[i],nums[j])) {
+                result = Math.max(nums[i]^nums[j],result);
+            }
+        }
+    }
+    return result;
+};
