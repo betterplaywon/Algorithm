18eddac7fdec9859fe5142cbe12902e06bf2496b
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-07-08 21:31:05 +0900

    Time: 82 ms (19.46%), Space: 59.3 MB (5.31%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    const arr = [];
+    let min = Infinity;
+
+const traverse = (root) => {
+if(!root) return;
+traverse(root.left);
+arr.push(root.val);
+traverse(root.right);
+}
+traverse(root);
+console.log({arr})
+for(let i=1; i<arr.length; i++) {
+    min = Math.min(min, arr[i]-arr[i-1]);
+}
+return min;
+};
