b100bcf3fcaf19ac13794c7e97379b0eddc50b84
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-10-11 07:34:29 +0900

    Time: 51 ms (87.98%), Space: 51.2 MB (72.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-
-    let result;
-    nums.sort((a,b) => a-b);
-    
-    const numsLength = nums.length + 1;
-    
-    for(let i=0; i < numsLength; i++) {
-        if(!nums.includes(i)) result = i;
+    let idxSum = 0, numsSum = 0;
+    const length =nums.length;
+    for(let i=0; i<length; i++) {
+        idxSum+=i;
+        numsSum+=nums[i];
     }
-    return result;
+    return (idxSum+length)-numsSum;
 };
