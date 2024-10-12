07f14baa29ccb7568c823f2320719aa1196eff56
강지원 <78709765+betterplaywon@users.noreply.github.com>
2024-10-12 15:14:42 +0900

    Time: 46 ms (82.2%), Space: 49 MB (14.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,11 +3,16 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
-    let dp=[1,1];
-    
-    for(let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp[n]
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+
+   let STEP_ONE = 1, STEP_TWO = 2;
+   let TOTAL_STEP = 0;
+
+   for(let i=3; i<=n; i++) {
+    TOTAL_STEP = STEP_ONE + STEP_TWO; // 5 기준. 3-> 5 -> 8
+    STEP_ONE = STEP_TWO; // 0 -> 2 -> 2
+    STEP_TWO = TOTAL_STEP; // 0 -> 3 -> 5
+   }
+   return TOTAL_STEP;
 };
